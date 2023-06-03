02a582f92aec26a36f33af4294828084c7c1d622
safayet <safayetdib@gmail.com>
2023-06-04 03:48:59 +0600

    added delete task function

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // mongoDB starts
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dc2rluc.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -47,6 +47,14 @@ async function run() {
 			const filter = req.query.filter;
 			const query = { status: filter };
 			const result = await taskCollection.find(query).toArray();
+			res.send(result);
+		});
+
+		// delete a task
+		app.delete('/task/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await taskCollection.deleteOne(query);
 			res.send(result);
 		});
 
