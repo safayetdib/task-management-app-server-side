d2c83dc96ca42e5e998051e7d18a51b95fd2febf
safayet <safayetdib@gmail.com>
2023-06-03 21:15:20 +0600

    added mongodb

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongoDB starts
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dc2rluc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		await client.connect();
+
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		await client.close();
+	}
+}
+run().catch(console.dir);
+
+// mongoDB ends
+
 app.get('/', (req, res) => {
 	res.send('Task Manager server is working...');
 });
